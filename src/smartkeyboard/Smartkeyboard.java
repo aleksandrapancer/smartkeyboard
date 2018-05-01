c70130e42211dac5a5c91f974af3c32db2b19190
Aleksandra Pancer <pancer.aleksandra@gmail.com>
2018-05-01 12:05:10 +0200

    changing layout implemented

--- src/smartkeyboard/Smartkeyboard.java
+++ src/smartkeyboard/Smartkeyboard.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
@@ -16,39 +17,51 @@ public class Smartkeyboard extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-
+    
+    final VBox root = new VBox(5);
+    root.setPadding(new Insets(10));
+    Scene scene = new Scene(root);  
+      
+   
     final TextArea textArea = new TextArea();
-    final Button okButton = new Button("OK");
-    okButton.setDefaultButton(true);
-    okButton.setOnAction(new EventHandler<ActionEvent>() {
+    final Button defaultButton = new Button("Domyślny układ QWERTY");
+    defaultButton.setDefaultButton(true);
+    defaultButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
-        System.out.println("OK Button Pressed!");
+        System.out.println("Domyślny układ klawiatury QWERTY");
+        root.getChildren().remove(2);         
+        VirtualKeyboard vkb = new VirtualKeyboard(1);
+        vkb.view().setStyle("-fx-border-color: darkblue; -fx-border-radius: 5;");
+
+        root.getChildren().add(vkb.view()); 
       }
     });
     
-    final Button cancelButton = new Button("Cancel");
-    cancelButton.setCancelButton(true);
-    cancelButton.setOnAction(new EventHandler<ActionEvent>() {
+    final Button customButton = new Button("Spersonalizowana układ klawiatury");
+    customButton.setCancelButton(true);
+    customButton.setOnAction(new EventHandler<ActionEvent>() {
       @Override
       public void handle(ActionEvent event) {
-        System.out.println("Canceled!");
+        System.out.println("Spersonalizowany układ klawiatury");
+        
+        root.getChildren().remove(2);         
+        VirtualKeyboard vkb = new VirtualKeyboard(2);
+        vkb.view().setStyle("-fx-border-color: darkblue; -fx-border-radius: 5;");
+
+        root.getChildren().add(vkb.view()); 
       }
     });
     
     final CheckBox editModeCheckBox = new CheckBox("EDIT MODE ACTIVE");
     
     final HBox buttons = new HBox(5);
-    buttons.getChildren().addAll(okButton, cancelButton, editModeCheckBox);
+    buttons.getChildren().addAll(defaultButton, customButton, editModeCheckBox);
     buttons.setAlignment(Pos.CENTER);
-
-    final VBox root = new VBox(5);
-    root.setPadding(new Insets(10));
-    Scene scene = new Scene(root);
-    
-    VirtualKeyboard vkb = new VirtualKeyboard();
+   
+    VirtualKeyboard vkb = new VirtualKeyboard(1);
     vkb.view().setStyle("-fx-border-color: darkblue; -fx-border-radius: 5;");
-    
+
     root.getChildren().addAll(textArea, buttons, vkb.view());
 
     primaryStage.setScene(scene);
